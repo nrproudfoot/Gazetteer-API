936aa50f67df8fc887f7ffd08fb7f73f84b70809
nathan89 <nathan.proudfoot@itvet.co.uk>
2023-01-29 01:08:41 +0000

    Fix covid formatting logic

--- utils/covid.util.ts
+++ utils/covid.util.ts
@@ -1,18 +1,22 @@
 import { ICovid } from '../models/data.model';
 import { ICovidResponse } from '../models/responses.model';
 
-export const formatCovid = (covid: ICovidResponse[]): ICovid => ({
-    latest: {
-        confirmed: covid.reverse()[0].Confirmed,
-        deaths: covid.reverse()[0].Deaths,
-        weeklyConfirmed: covid.reverse()[0].Confirmed - covid.reverse()[6].Confirmed,
-        weeklyDeaths: covid.reverse()[0].Deaths - covid.reverse()[6].Deaths,
-        dailyAverage: covid.reverse()[0].Confirmed - covid.reverse()[6].Confirmed / 7,
-        dailyDeaths: covid.reverse()[0].Deaths - covid.reverse()[6].Confirmed / 7
-    },
-    daily: covid.reverse().slice(0, 14).map((data, index) => ({
-        cases: index !== 13 ? data.Confirmed - covid.reverse()[index + 1].Confirmed : 0,
-        deaths: index !== 13 ? data.Deaths - covid.reverse()[index + 1].Deaths : 0,
-        date: data.Date
-    })).slice(0, 13)
-}) 
+export const formatCovid = (covid: ICovidResponse[]): ICovid | undefined => {
+    if (!covid) return undefined;
+    covid.reverse();
+    return {
+        latest: {
+            confirmed: covid[0].Confirmed,
+            deaths: covid[0].Deaths,
+            weeklyConfirmed: covid[0].Confirmed - covid[6].Confirmed,
+            weeklyDeaths: covid[0].Deaths - covid[6].Deaths,
+            dailyAverage: (covid[0].Confirmed - covid[6].Confirmed) / 7,
+            dailyDeaths: (covid[0].Deaths - covid[6].Deaths) / 7
+        },
+        daily: covid.slice(0, 14).map((data, index) => ({
+            cases: index !== 13 ? data.Confirmed - covid[index + 1].Confirmed : 0,
+            deaths: index !== 13 ? data.Deaths - covid[index + 1].Deaths : 0,
+            date: data.Date
+        })).slice(0, 13)
+    }
+} 
